bbe785c3cd6f76fabc96af57f636b72b3a4f1c35
Benta <bentaakitwi500@gmail.com>
2023-03-24 18:17:40 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
+
   // your code here
+  let submitForm = document.querySelector('form')
+  submitForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleToDo(e.target['new-task-description'].value)
+  }) 
 });
+
+function handleToDo(todo) {
+  let li = document.createElement('li');
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+
+  p.textContent = todo;
+  btn.textContent = 'x';
+
+  li.appendChild(p);
+  li.appendChild(btn);
+  document.querySelector('#tasks').appendChild(li);
+
+  btn.addEventListener('click', function() {
+    li.remove();
+  });
+}
+
